27ece742344d07d902dfb33c481348ff9d8854b4
nayaknishant656 <nayaknishant656@gmail.com>
2026-02-07 21:56:15 +0530

    authentication api configured

--- task-frotend/src/config.js
+++ task-frotend/src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5002/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://task-media-app-backend.vercel.app/api';
 
 export default API_BASE_URL;
